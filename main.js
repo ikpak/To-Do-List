6fa890329bf335f708ce1c7ad44f8389c5f35899
ikpak <inkha.pak@gmail.com>
2020-05-19 17:35:24 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+let itemList = []
+
+let addItem = () => {
+    let item = {
+        text: document.getElementById("itemInput").value,
+        isDone: false
+    }
+    itemList.push(item)
+    showList(itemList)
+}
+
+let removeItem = (i) => {
+    itemList.splice(i, 1)
+    showList(itemList)
+}
+
+let showList = (list) => {
+    let message = list.map((item, i) => {
+        if(item.isDone) {
+            return `<li><input type="checkbox" onclick="toggle(${i})" checked />${item.text}<i class="fas fa-trash-alt" onclick='removeItem(${i})'></i></li>`.strike()
+        } else {
+            return `<li><input type="checkbox" onclick="toggle(${i})" />${item.text}<i class="fas fa-trash-alt" onclick='removeItem(${i})'></i></li>`
+        } 
+    }).join('')
+        
+    document.getElementById("itemListArea").innerHTML = message
+}
+
+let toggle = (i) => {
+    itemList[i].isDone = !(itemList[i].isDone)
+    showList(itemList)
+}
